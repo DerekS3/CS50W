379664257fb8edab4c1a683af9596d0a93c69442
DS0eng <derekstrickland3@gmail.com>
2025-01-01 20:47:51 +0000

    linked posts.js

---
+++ network/network/static/network/posts.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Use buttons to toggle between views
+    //document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
+    document.querySelector('#post-content').style.display = 'none';
+  
+    // By default, load the inbox
+    //load_mailbox('inbox');
+  });
